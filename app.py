4de0dac77879cdae73c005463262d28ef3999321
theMentalItchGuy <tushar240688@gmail.com>
2015-08-31 01:20:42 +0530

    Fix from View function does not return a value

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from stop_words import stops
 
 
 
+
 #
 # Configurations
 #
@@ -70,7 +71,7 @@ def index():
                 db.session.commit()
             except:
                 errors.append("Unable to add item to database")
-        return render_template('index.html', errors=errors, results=results)
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
